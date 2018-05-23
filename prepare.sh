b2a0f3b451add0ed875f07ce5719ad430d4e4c93
liyanguo <lyg@meitu.com>
2018-05-23 18:48:13 +0800

    fix

--- prepare.sh
+++ prepare.sh
@@ -8,8 +8,9 @@ mkdir -p $GOPATH/src/golang.org/x && cd $GOPATH/src/golang.org/x && git clone ht
 cd ${GOPATH}
 go get -u github.com/samuel/go-zookeeper/zk
 go get -u github.com/go-sql-driver/mysql
-go get -u gopkg.in/yaml.v2
+go get -u -insecure gopkg.in/yaml.v2
 go get -u github.com/afocus/captcha
-go get -u golang.org/x/crypto/ssh
 
-# git clone https://github.com/golang/crypto.git && cp target to golang.org
+# go get -u golang.org/x/crypto/ssh
+git clone https://github.com/golang/crypto.git
+cp -rf ${GOPATH}/crypto ${GOPATH}/src/golang.org/x
--- src/monitor/global/variable.go
+++ src/monitor/global/variable.go
@@ -3,10 +3,7 @@ package global
 import (
 	"monitor/config"
 	"mt/mtlog"
-	"golang.org/x/crypto/ssh"
 )
 
 var Conf *config.Conf = nil
 var Logger *mtlog.Logger = nil
-var MailReceivers = "lyg@meitu.com"
-
